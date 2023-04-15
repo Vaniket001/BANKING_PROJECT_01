e7d1763dac0e3b2d50a6f9566215b8471565f4a2
Aniket <kumaraniket900@gmail.com>
2023-04-15 17:41:41 +0530

    delete controller added

--- src/main/java/com/bank/controller/BankDetailsController.java
+++ src/main/java/com/bank/controller/BankDetailsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,12 @@ public class BankDetailsController {
 	@GetMapping("{id}")
 	public ResponseEntity<BankInfo> getBankInfoById(@PathVariable("id") String bankId) throws Exception{
 		return new ResponseEntity<>(bankDetailsService.getBankInfo(bankId), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<BankInfo> deleteBankInfoById(@PathVariable("id") String bankId) throws Exception{
+		// Delete service will be called...
+		return null;
 	}
 
 }
